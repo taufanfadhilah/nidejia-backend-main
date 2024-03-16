812e52a05e84ef26e265ef1f902ed0e36080f9ec
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2024-03-17 01:10:24 +0700

    feat: dashboard

---
+++ app/Filament/Widgets/WaitingTransactions.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Tables\Actions\Action;
+use Filament\Notifications\Notification;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+use App\Models\Transaction;
+
+class WaitingTransactions extends BaseWidget
+{
+    protected static ?int $sort = 3;
+    protected int|string|array $columnSpan = '6';
+    
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Transaction::query()->whereStatus('waiting')
+            )
+            ->columns([
+                Tables\Columns\TextColumn::make('User.name'),
+                Tables\Columns\TextColumn::make('Listing.title'),
+                Tables\Columns\TextColumn::make('total_price')->money('USD'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'waiting' => 'gray',
+                        'approved' => 'info',
+                        'canceled' => 'danger',
+                    })
+            ])->actions([
+                    Action::make('approve')
+                        ->button()
+                        ->color('success')
+                        ->requiresConfirmation()
+                        ->action(function (Transaction $transaction) {
+                            Transaction::find($transaction->id)->update([
+                                'status' => 'approved'
+                            ]);
+                            Notification::make()->success()->title('Transaction Approved!')->body('Transaction has been approved successfully')->icon('heroicon-o-check')->send();
+                        })
+                        ->hidden(fn(Transaction $transaction) => $transaction->status !== 'waiting')
+                ]);
+    }
+}
