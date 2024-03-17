f66fa4e1fdbba3117501ab4f6bbae212b16446cd
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2024-03-17 13:13:02 +0700

    feat: add validateCsrfTokens

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Session\Middleware\StartSession::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            '*',
+        ]);
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
